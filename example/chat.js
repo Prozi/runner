c68d2f3ef600af70a39d65488554a096e61973d0
Jacek Pietal <jpietal@7bulls.com>
2017-05-13 04:15:24 +0200

    prepare to publish

---
+++ example/chat.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// it eats this format
+module.exports = {
+    initialize(io) {
+        this.io = io;
+        console.log('Initialized socket io');
+    },
+    handshake(socket, data) {
+        console.log('Handshaken socket', socket.id, data);
+        socket.emit('handshaken:example', {
+            example: 'data from server'
+        });
+        this.io.emit('joined', socket.id);
+    }
+};
--- example/index.js
+++ example/index.js
@@ -1,24 +1,9 @@
 'use strict';
 
-const config = require('./config');
 const run = require('../lib');
-
-// it eats this format
-const example = {
-    initialize(io) {
-        this.io = io;
-        console.log('Initialized socket io');
-    },
-    handshake(socket, data) {
-        console.log('Handshaken socket', socket.id, data);
-        socket.emit('handshaken:example', {
-            example: 'data from server'
-        });
-        this.io.emit('joined', socket.id);
-    }
-};
-
-const plugins = { example };
+const config = require('./config');
+const chat = require('./chat');
+const plugins = { chat };
 
 run({
     config,
