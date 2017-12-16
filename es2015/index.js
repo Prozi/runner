13af4c786a4d3df14b17dfd6512846e8d6847fd1
Jacek Pietal <prozi85@gmail.com>
2017-12-16 23:12:07 +0100

    rfc/removed-passport-dependency update~dependencies2latest

--- es2015/index.js
+++ es2015/index.js
@@ -1,6 +1,33 @@
 'use strict';
 
-module.exports = function run(_ref) {
+var express = require('express');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var http = require('http');
+var cors = require('cors');
+var compression = require('compression');
+var socketio = require('socket.io');
+var MongoStore = require('connect-mongo')(expressSession);
+var sticky = require('sticky-cluster');
+var stamp = require('console-stamp');
+var port = process.env.PORT || 3000;
+
+module.exports = function (params) {
+  return sticky(function (callback) {
+    callback(run(params));
+  }, {
+    concurrency: params.config.workers || 1,
+    port: port,
+    debug: true,
+    env: function env(index) {
+      return { stickycluster_worker_index: index };
+    }
+  });
+};
+
+function run(_ref) {
   var _ref$config = _ref.config,
       config = _ref$config === undefined ? {} : _ref$config,
       _ref$plugins = _ref.plugins,
@@ -9,89 +36,43 @@ module.exports = function run(_ref) {
       extend = _ref$extend === undefined ? null : _ref$extend;
 
 
-  var passportSocketIo = require('passport.socketio');
-  var express = require('express');
-  var expressSession = require('express-session');
-  var bodyParser = require('body-parser');
-  var cookieParser = require('cookie-parser');
-  var path = require('path');
-  var http = require('http');
-  var cors = require('cors');
-  var compression = require('compression');
-  var socketio = require('socket.io');
-  var passport = require('passport');
-  var MongoStore = require('connect-mongo')(expressSession);
-  var sticky = require('sticky-session');
-  var stamp = require('console-stamp');
-
   var app = express();
-  var port = process.env.PORT || 3000;
-  var store = new MongoStore(config.store);
   var cookie = cookieParser(config.auth.secret);
-  var session = expressSession({
-    key: config.auth.key || 'express.sid',
-    secret: config.auth.secret,
-    cookie: config.auth.cookie,
-    resave: true,
-    saveUninitialized: true,
-    store: store
-  });
+  var store = new MongoStore(config.store);
 
-  stamp(console, {
-    metadata: config.metadata ? metadata : null,
-    colors: config.colors || {}
-  });
+  var sessionParams = getSessionParams(store, config);
+  var session = expressSession(sessionParams);
 
-  // cors
+  var stampParams = getStampParams(config);
+  stamp(console, stampParams);
+
   app.use(cors());
-
-  // server static public dir
-  app.set('view engine', 'html');
-  config.static.directories.forEach(function (directory) {
-    app.use(express.static(path.resolve(directory), config.static.config));
-  });
+  app.use(compression());
 
   app.use(session);
   app.use(cookie);
 
-  // for passport
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  app.use(compression());
+  serveStatic(app, config);
 
   // optional extend app
   if (extend) {
     extend(app);
   }
 
-  passport.serializeUser(serializeUser);
-  passport.deserializeUser(serializeUser);
-
   var server = http.Server(app);
   var io = socketio.listen(server);
 
   io.set('origins', '*:*');
-
-  // socket-passport integration
-  io.use(passportSocketIo.authorize({
-    cookieParser: cookieParser.bind(null, config.auth.secret),
-    key: config.auth.key,
-    secret: config.auth.secret,
-    store: store
-  }));
 
   Object.keys(plugins).forEach(function (name) {
     plugins[name].initialize(io.in(name));
   });
 
   io.on('connect', function (socket) {
-    console.info('[socket] connect', socket.id);
+    console.log('[socket] connect', socket.id);
     // require custom handshake
     Object.keys(plugins).forEach(function (name) {
       socket.on('handshake:' + name, function (data) {
@@ -101,13 +82,36 @@ module.exports = function run(_ref) {
     });
   });
 
-  function metadata() {
-    return '[' + process.memoryUsage().rss + ']';
-  }
+  return server;
+}
 
-  function serializeUser(user, done) {
-    done(null, user);
-  }
+function getSessionParams(store, config) {
+  return {
+    key: config.auth.key || 'express.sid',
+    secret: config.auth.secret,
+    cookie: config.auth.cookie,
+    resave: true,
+    saveUninitialized: true,
+    store: store
+  };
+}
 
-  return sticky.listen(server, port, { workers: config.workers });
-};
+function getStampParams(config) {
+  var metadata = typeof config.metadata === 'function' ? config.metadata : config.metadata ? defaultMetadata : null;
+  return {
+    metadata: metadata,
+    colors: config.colors || {}
+  };
+}
+
+function serveStatic(app, config) {
+  // server static public dir
+  app.set('view engine', 'html');
+  config.static.directories.forEach(function (directory) {
+    app.use(express.static(path.resolve(directory), config.static.config));
+  });
+}
+
+function defaultMetadata() {
+  return '[' + process.memoryUsage().rss + ']';
+}
--- source/index.js
+++ source/index.js
@@ -1,111 +1,118 @@
-'use strict';
+'use strict'
 
-module.exports = function run({
+const express = require('express')
+const expressSession = require('express-session')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const http = require('http')
+const cors = require('cors')
+const compression = require('compression')
+const socketio = require('socket.io')
+const MongoStore = require('connect-mongo')(expressSession)
+const sticky = require('sticky-cluster')
+const stamp = require('console-stamp')
+const port = process.env.PORT || 3000
+
+module.exports = function (params) {
+  return sticky(
+    function (callback) {
+      callback(run(params))
+    }, {
+      concurrency: params.config.workers || 1,
+      port,
+      debug: true,
+      env: function (index) { return { stickycluster_worker_index: index } }
+    }
+  )
+}
+
+function run({
   config = {},
   plugins = {},
   extend = null
 }) {
 
-  const passportSocketIo = require('passport.socketio');
-  const express = require('express');
-  const expressSession = require('express-session');
-  const bodyParser = require('body-parser');
-  const cookieParser = require('cookie-parser');
-  const path = require('path');
-  const http = require('http');
-  const cors = require('cors');
-  const compression = require('compression');
-  const socketio = require('socket.io');
-  const passport = require('passport');
-  const MongoStore = require('connect-mongo')(expressSession);
-  const sticky = require('sticky-session');
-  const stamp = require('console-stamp');
+  const app = express()
+  const cookie = cookieParser(config.auth.secret)
+  const store = new MongoStore(config.store)
 
-  const app = express();
-  const port = process.env.PORT || 3000;
-  const store = new MongoStore(config.store);
-  const cookie = cookieParser(config.auth.secret);
-  const session = expressSession({
+  const sessionParams = getSessionParams(store, config)
+  const session = expressSession(sessionParams)
+
+  const stampParams = getStampParams(config)
+  stamp(console, stampParams)
+
+  app.use(cors())
+  app.use(compression())
+
+  app.use(session)
+  app.use(cookie)
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+
+  serveStatic(app, config)
+
+  // optional extend app
+  if (extend) {
+    extend(app)
+  }
+
+  const server = http.Server(app)
+  const io = socketio.listen(server)
+
+  io.set('origins', '*:*')
+
+  Object.keys(plugins).forEach((name) => {
+    plugins[name].initialize(io.in(name))
+  })
+
+  io.on('connect', (socket) => {
+    console.log('[socket] connect', socket.id)
+    // require custom handshake
+    Object.keys(plugins).forEach((name) => {
+      socket.on(`handshake:${name}`, (data) => {
+        socket.join(name)
+        plugins[name].handshake(socket, data)
+      })
+    })
+  })
+
+  return server
+}
+
+function getSessionParams (store, config) {
+  return {
     key: config.auth.key || 'express.sid',
     secret: config.auth.secret,
     cookie: config.auth.cookie,
     resave: true,
     saveUninitialized: true,
     store
-  });
+  }
+}
 
-  stamp(console, {
-    metadata: config.metadata ? metadata : null,
+function getStampParams (config) {
+  const metadata = (typeof config.metadata === 'function') ? 
+    config.metadata : 
+    config.metadata ?
+      defaultMetadata :
+      null
+  return {
+    metadata,
     colors: config.colors || {}
-  });
+  }
+}
 
-  // cors
-  app.use(cors());
-
+function serveStatic (app, config) {
   // server static public dir
-  app.set('view engine', 'html');
+  app.set('view engine', 'html')
   config.static.directories.forEach((directory) => {
-    app.use(express.static(path.resolve(directory), config.static.config));
-  });
+    app.use(express.static(path.resolve(directory), config.static.config))
+  })
+}
 
-  app.use(session);
-  app.use(cookie);
-
-  // for passport
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  app.use(compression());
-
-  // optional extend app
-  if (extend) {
-    extend(app);
-  }
-
-  passport.serializeUser(serializeUser);
-  passport.deserializeUser(serializeUser);
-
-  const server = http.Server(app);
-  const io = socketio.listen(server);
-
-  io.set('origins', '*:*');
-
-  // socket-passport integration
-  io.use(passportSocketIo.authorize({
-    cookieParser: cookieParser.bind(null, config.auth.secret),
-    key: config.auth.key,
-    secret: config.auth.secret,
-    store
-  }));
-
-  Object.keys(plugins).forEach((name) => {
-    plugins[name].initialize(io.in(name));
-  });
-
-  io.on('connect', (socket) => {
-    console.info('[socket] connect', socket.id);
-    // require custom handshake
-    Object.keys(plugins).forEach((name) => {
-      socket.on(`handshake:${name}`, (data) => {
-        socket.join(name);
-        plugins[name].handshake(socket, data);
-      });
-    });
-  });
-
-  function metadata() {
-    return '[' + process.memoryUsage().rss + ']';
-  }
-
-  function serializeUser(user, done) {
-    done(null, user);
-  }
-
-  return sticky.listen(server, port, { workers: config.workers });
-
-};
+function defaultMetadata() {
+  return '[' + process.memoryUsage().rss + ']'
+}
